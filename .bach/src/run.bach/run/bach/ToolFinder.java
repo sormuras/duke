04cbe3e2c2d471644bb4e64741bacda692cdbb31
Christian Stein <sormuras@gmail.com>
2022-10-02 08:55:35 +0200

    Mark tool finder as a functional interface

--- .bach/src/run.bach/run/bach/ToolFinder.java
+++ .bach/src/run.bach/run/bach/ToolFinder.java
@@ -3,17 +3,21 @@ package run.bach;
 import java.util.List;
 import java.util.Optional;
 
+@FunctionalInterface
 public interface ToolFinder {
-  String description();
-
   List<Tool> findAll();
 
   default Optional<Tool> findFirst(String string) {
     return findAll().stream().filter(tool -> tool.matches(string)).findFirst();
   }
 
+  default String description() {
+    return getClass().getSimpleName();
+  }
+
   static ToolFinder of(String description, List<Tool> tools) {
-    record ListToolFinder(String description, List<Tool> findAll) implements ToolFinder {}
     return new ListToolFinder(description, List.copyOf(tools));
   }
+
+  record ListToolFinder(String description, List<Tool> findAll) implements ToolFinder {}
 }
