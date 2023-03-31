acd47bfc08cb9cc01bdebdf3a6381ee9ab87fab9
Christian Stein <sormuras@gmail.com>
2023-03-31 08:45:31 +0200

    Add Duke's Java program and JShell interface

---
+++ src/duke.run/Duke.java
@@ -0,0 +1,8 @@
+/** Duke's Java program and JShell interface. */
+public interface Duke {
+  String VERSION = "2023.03.31+08.36";
+
+  static void main(String[] args) {
+    System.out.println("Duke.java " + VERSION);
+  }
+}
