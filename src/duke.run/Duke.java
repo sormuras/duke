455440a9d2d0008ceba890a9ed12ba337b61af57
Christian Stein <sormuras@gmail.com>
2023-04-01 18:35:10 +0200

    Update version in `Duke.java`

--- src/duke.run/Duke.java
+++ src/duke.run/Duke.java
@@ -1,6 +1,6 @@
 /** Duke's Java program and JShell interface. */
 public interface Duke {
-  String VERSION = "2023.03.31+08.36";
+  String VERSION = "2023.04.11+18.34";
 
   static void main(String[] args) {
     System.out.println("Duke.java " + VERSION);
