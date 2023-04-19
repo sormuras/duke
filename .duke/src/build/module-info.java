be21e8d16046c4437ec54cbe6dee9836183d55d4
Christian Stein <sormuras@gmail.com>
2023-04-19 15:50:11 +0200

    Use `border` module and tool in `build` module and tool

--- .duke/src/build/module-info.java
+++ .duke/src/build/module-info.java
@@ -1,8 +1,16 @@
+import jdk.tools.Task;
 import jdk.tools.ToolInstaller.Setup;
 
+@Task.Of(
+    name = "b",
+    args = {"build"})
+@Task.Of(
+    name = "bb",
+    args = {"border", "BEGIN", "+", "format", "+", "build", "+", "border", "END."})
 @Setup(service = run.duke.store.GoogleJavaFormatInstaller.class, version = "1.16.0")
 @Setup(service = run.duke.store.EchoInstaller.class, version = "99")
 module build {
+  requires border;
   requires jdk.tools;
   requires run.duke;
   requires run.duke.store;
